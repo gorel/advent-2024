0bb016ed331409e2e5cb273ba413d23b442ae555
Logan Gore <lgore93@gmail.com>
2024-12-01 00:08:39 -0500

    Day1

--- advent/day01.py
+++ advent/day01.py
@@ -2,16 +2,31 @@
 
 from __future__ import annotations
 
+import collections
+
 from advent.base import BaseSolver, Solution
 
 
 class Solver(BaseSolver):
     def solve(self) -> Solution:
+        ls = []
+        rs = []
         for line in self.lines:
-            print(line)
+            l, r = line.split()
+            ls.append(int(l))
+            rs.append(int(r))
+        ls.sort()
+        rs.sort()
+        d = collections.Counter(rs)
 
-        yield None
-        yield None
+        part1 = 0
+        part2 = 0
+        for i in range(len(ls)):
+            part1 += abs(ls[i] - rs[i])
+            part2 += d[ls[i]] * ls[i]
+
+        yield part1
+        yield part2
 
 
 Solver.run()
