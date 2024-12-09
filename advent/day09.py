6b45102e63654287268dc6a350adaf800ca17a61
Logan Gore <lgore93@gmail.com>
2024-12-09 10:44:50 -0800

    Day9

--- advent/day09.py
+++ advent/day09.py
@@ -2,70 +2,160 @@
 
 from __future__ import annotations
 
+import dataclasses
+from typing import Iterator
+
 from advent.base import BaseSolver, Solution
 
 
-def calculate(
-    vals: list[tuple[int | None, int]], move_blocks: bool = False
-) -> tuple[str, int]:
-    s = ""
-    res = 0
-    block_idx = 0
-    l_idx = 0
-    while l_idx < len(vals):
-        val, run = vals[l_idx]
-        # Move items from the right
-        if val is None:
-            remaining = run
-            r_idx = len(vals) - 1
-            while remaining and l_idx <= r_idx:
-                rval, rrun = vals[r_idx]
-                if rval is None or rrun == 0:
-                    r_idx -= 1
+@dataclasses.dataclass
+class Node:
+    file_idx: int | None
+    length: int
+    prev: Node | None = None
+    next: Node | None = None
+    tail: Node | None = None
+
+    @classmethod
+    def from_runs(cls, runs: list[int]) -> Node:
+        head = Node(-1, -1)
+        cur = head
+        for idx, run in enumerate(runs):
+            if idx % 2 == 0:
+                cur = cur.append(Node(idx // 2, run))
+            else:
+                cur = cur.append(Node(None, run))
+        head.tail = cur.append(Node(-1, -1))
+        return head
+
+    def prepend(self, node: Node) -> Node:
+        node.next = self
+        node.prev = self.prev
+        if self.prev is not None:
+            self.prev.next = node
+        self.prev = node
+        return node
+
+    def append(self, node: Node) -> Node:
+        node.next = self.next
+        self.next = node
+        node.prev = self
+        return node
+
+    def defrag(self) -> None:
+        for node in self:
+            if node.file_idx is None:
+                assert self.tail is not None
+                rcur = self.tail.prev
+                assert rcur is not None
+                while node.length > 0 and rcur is not None and rcur is not node:
+                    if rcur.file_idx is None or rcur.length == 0:
+                        rcur = rcur.prev
+                        continue
+
+                    to_move = min(node.length, rcur.length)
+                    # Now we *prepend* this node with our filled value
+                    # And set the new length to remaining - to_move
+                    node.prepend(Node(rcur.file_idx, to_move))
+                    rcur.length -= to_move
+                    node.length -= to_move
+
+                    # Lastly: rcur has None added to its *end*
+                    if rcur.next is not self.tail:
+                        assert rcur.next is not None
+                        if rcur.next.file_idx is None:
+                            rcur.next.length += to_move
+                        else:
+                            rcur.append(Node(None, to_move))
+                    rcur = rcur.prev
+
+    def defrag2(self) -> None:
+        moved = set()
+        assert self.tail is not None
+        rcur = self.tail.prev
+        assert rcur is not None
+        while rcur is not self:
+            assert rcur is not None
+            # If this is empty or of zero length, count it as "solved"
+            if rcur.file_idx is None or rcur.length == 0 or rcur.file_idx in moved:
+                rcur = rcur.prev
+                continue
+
+            for node in self:
+                # First fit is *after* the block, which would make fragmentation worse
+                if node is rcur:
+                    break
+
+                # Node isn't empty -> obviously can't move here
+                if node.file_idx is not None:
                     continue
 
-                if move_blocks and rrun > remaining:
-                    r_idx -= 1
+                # Block won't fit here, try the next one
+                if rcur.length > node.length:
                     continue
 
-                to_move = min(remaining, rrun)
-                vals[r_idx] = (rval, rrun - to_move)
-                # sum from b = block_idx to block_idx + to_move of b * rval
-                res += sum(b * rval for b in range(block_idx, block_idx + to_move))
-                s += str(rval) * to_move
-                if vals[r_idx][-1] == 0:
-                    r_idx -= 1
-                block_idx += to_move
-                remaining -= to_move
-            s += "." * remaining
-            block_idx += remaining
-        else:
-            # Sum from b = block_idx to block_idx + run of b * val
-            res += sum(b * val for b in range(block_idx, block_idx + run))
-            block_idx += run
-            s += str(val) * run
-        l_idx += 1
-    return s, res
+                # Move the block here - this is equivalent to prepending the rcur node
+                # and reducing this node's length accordingly
+                node.prepend(Node(rcur.file_idx, rcur.length))
+                node.length -= rcur.length
+                moved.add(rcur.file_idx)
+                rcur.file_idx = None
+                # Merge it with the previous if they're both None now
+                assert rcur.prev is not None
+                if rcur.prev.file_idx is None:
+                    to_add = rcur.length
+                    next = rcur.next
+                    # If the next is *also* None, they all get combined
+                    assert rcur.next is not None
+                    if rcur.next.file_idx is None:
+                        to_add = rcur.length + rcur.next.length
+                        next = rcur.next.next
+                    rcur.prev.length += to_add
+                    rcur.prev.next = next
+                    assert next is not None
+                    next.prev = rcur.prev
+                break
 
+            # Move onto the next block for defragging
+            assert rcur is not None
+            rcur = rcur.prev
 
-def runs_to_vals(runs: list[int]) -> list[tuple[int | None, int]]:
-    vals = []
-    for idx, run in enumerate(runs):
-        if idx % 2 == 0:
-            vals.append((idx // 2, run))
-        else:
-            vals.append((None, run))
-    return vals
+    def checksum(self) -> int:
+        res = 0
+        block_idx = 0
+        for node in self:
+            # Sum from block_idx to block_idx + cur.length of block_idx * cur.file_idx
+            if node.file_idx is not None:
+                res += sum(
+                    node.file_idx * b for b in range(block_idx, block_idx + node.length)
+                )
+            block_idx += node.length
+        return res
+
+    def __iter__(self) -> Iterator[Node]:
+        cur = self.next
+        while cur is not None and cur.length != -1:
+            yield cur
+            cur = cur.next
+
+    def __str__(self) -> str:
+        return "".join(
+            (str(node.file_idx) if node.file_idx is not None else ".") * node.length
+            for node in self
+        )
 
 
 class Solver(BaseSolver):
     def solve(self) -> Solution:
         runs = [int(c) for c in self.data.strip()]
 
-        _, part1 = calculate(runs_to_vals(runs))
-        _, part2 = calculate(runs_to_vals(runs), move_blocks=True)
-        yield part1
-        yield part2
+        part1 = Node.from_runs(runs)
+        part1.defrag()
+        yield part1.checksum()
+
+        part2 = Node.from_runs(runs)
+        part2.defrag2()
+        yield part2.checksum()
 
 
 Solver.run()
