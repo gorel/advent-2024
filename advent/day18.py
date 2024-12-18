5891edb5b8e3777bb30699af65767a86dbcab4e5
Logan Gore <lgore93@gmail.com>
2024-12-18 11:43:25 -0800

    Binary search

--- advent/day18.py
+++ advent/day18.py
@@ -8,39 +8,39 @@ from advent.base import BaseSolver, Solution
 
 
 class Solver(BaseSolver):
-    def solve(self) -> Solution:
-        g = nx.Graph()
-
-        size = 71 if self.is_real else 7
-        first = 1024 if self.is_real else 12
+    def build_graph(self, size: int, n: int) -> nx.Graph:
         points = set()
-        for line in self.lines[:first]:
+        for line in self.lines[:n]:
             x, y = line.split(",")
             points.add((int(x), int(y)))
-
-        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        g = nx.Graph()
         for x in range(size):
             for y in range(size):
                 if (x, y) in points:
                     continue
-                for dx, dy in dirs:
+                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                     xx, yy = x + dx, y + dy
                     if 0 <= xx < size and 0 <= yy < size and (xx, yy) not in points:
                         g.add_edge((x, y), (xx, yy))
+        return g
+
+    def solve(self) -> Solution:
+        size = 71 if self.is_real else 7
+        n = 1024 if self.is_real else 12
+        g = self.build_graph(size, n)
         yield len(nx.shortest_path(g, (0, 0), (size - 1, size - 1))) - 1
 
-        for line in self.lines[first:]:
-            x, y = line.split(",")
-            px, py = int(x), int(y)
-            for dx, dy in dirs:
-                xx, yy = px + dx, py + dy
-                if 0 <= xx < size and 0 <= yy < size and (xx, yy) not in points:
-                    if g.has_edge((px, py), (xx, yy)):
-                        g.remove_edge((px, py), (xx, yy))
+        lo = n
+        hi = len(self.lines)
+        while lo < hi:
+            mid = (lo + hi) // 2
+            g = self.build_graph(size, mid)
             try:
                 nx.shortest_path(g, (0, 0), (size - 1, size - 1))
-            except nx.NetworkXNoPath:
-                yield line
+                lo = mid + 1
+            except Exception:
+                hi = mid
+        yield self.lines[lo - 1]
 
 
 Solver.run()
