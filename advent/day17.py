e0b616684719329517919fddcb6259d8d10a6ad4
Logan Gore <lgore93@gmail.com>
2024-12-17 16:48:13 -0800

    Solve without z3

--- advent/day17.py
+++ advent/day17.py
@@ -4,62 +4,61 @@ from __future__ import annotations
 
 import dataclasses
 
-import z3
-
 from advent.base import BaseSolver, Solution
 
 
 @dataclasses.dataclass
 class Computer:
     prog: list[int]
-    reg: dict[str, int]
+    a: int
+    b: int = 0
+    c: int = 0
     ip: int = 0
     out: list[int] = dataclasses.field(default_factory=list)
     matching: bool = False
-    start_a: int | None = None
 
-    def run(self, a: int | None = None) -> list[int]:
-        self.start_a = a
+    def match_one(self) -> int:
+        return self.run(match_one=True)[0]
+
+    def run(self, match_one: bool = False) -> list[int]:
         cache = set()
-        if a is not None:
-            self.reg["A"] = a
-            self.matching = True
 
         while self.ip < len(self.prog) - 1:
-            cache_key = (self.ip, self.reg["A"], self.reg["B"], self.reg["C"])
+            cache_key = (self.ip, self.a, self.b, self.c)
+            # To avoid an input that never halts
             if cache_key in cache:
                 return [-1]
             cache.add(cache_key)
             opcode = self.prog[self.ip]
             operand = self.prog[self.ip + 1]
             self.op(opcode, operand)
+            if match_one and len(self.out) > 0:
+                break
         return self.out
 
     def op(self, opcode: int, operand: int) -> None:
         match opcode:
             case 0:  # adv
-                self.reg["A"] = self.reg["A"] // 2 ** self.combo(operand)
+                self.a = self.a // 2 ** self.combo(operand)
             case 1:  # bxl
-                self.reg["B"] = self.reg["B"] ^ operand
+                self.b = self.b ^ operand
             case 2:  # bst
-                self.reg["B"] = self.combo(operand) % 8
+                self.b = self.combo(operand) % 8
             case 3:  # jnz
-                if self.reg["A"] != 0:
+                if self.a != 0:
                     self.ip = operand - 2
             case 4:  # bxc
-                self.reg["B"] = self.reg["B"] ^ self.reg["C"]
+                self.b = self.b ^ self.c
             case 5:  # out
                 self.out.append(self.combo(operand) % 8)
                 idx = len(self.out) - 1
                 if self.matching:
-                    if idx > 7:
-                        print(f"Got success at {idx} for starting a {self.start_a}")
                     if self.matching and self.out[idx] != self.prog[idx]:
                         self.ip = len(self.prog)
             case 6:  # bdv
-                self.reg["B"] = self.reg["A"] // 2 ** self.combo(operand)
+                self.b = self.a // 2 ** self.combo(operand)
             case 7:  # cdv
-                self.reg["C"] = self.reg["A"] // 2 ** self.combo(operand)
+                self.c = self.a // 2 ** self.combo(operand)
         self.ip += 2
 
         pass
@@ -67,13 +66,11 @@ class Computer:
     def combo(self, combo: int) -> int:
         match combo:
             case 4:
-                return self.reg["A"]
+                return self.a
             case 5:
-                return self.reg["B"]
+                return self.b
             case 6:
-                return self.reg["C"]
-            case 7:
-                exit("Encountered combo(7) - you messed up")
+                return self.c
             case _:
                 return combo
 
@@ -110,7 +107,7 @@ class Solver(BaseSolver):
             reg[parts[1].rstrip(":")] = int(parts[2])
 
         prog = [int(x) for x in program.split()[1].split(",")]
-        c = Computer(prog, reg)
+        c = Computer(prog, reg["A"])
         out = c.run()
         yield ",".join(str(i) for i in out)
 
@@ -119,28 +116,27 @@ class Solver(BaseSolver):
             res = []
             while res != prog:
                 a += 1
-                res = Computer(prog, reg).run(a)
+                res = Computer(prog, a).run()
             yield a
             return
 
-        opt = z3.Optimize()
-        # Assuming a fits in a 64-bit int
-        avec = z3.BitVec("a", 64)
-        a, b, c = avec, 0, 0
-        for expected in prog:
-            b = a % 8
-            b = b ^ 3
-            c = a / (1 << b)
-            b = b ^ 5
-            a = a / 8
-            b = b ^ c
-            opt.add((b % 8) == expected)
-        opt.add(a == 0)
-        opt.minimize(avec)
-        if opt.check() == z3.sat:
-            res = opt.model().eval(avec)
-            assert isinstance(res, z3.BitVecNumRef)
-            yield res.as_long()
+        # Prog[-i] only depends on the last three bits of A. Therefore, we can
+        # work backwards to generate the set of possible 3-bit A values that
+        # output the target value to build up our solution. We need to check
+        # *all* possibilities at each stage, because some bits may generate some
+        # of the last few outputs while not solving the first few. At the end,
+        # we just return the smallest input that yields the quine.
+        possibilities = {0}
+        for expected in prog[::-1]:
+            next_possibilities = set()
+            for res in possibilities:
+                next_possibilities |= {
+                    (res << 3 | c)
+                    for c in range(8)
+                    if Computer(prog, res << 3 | c).match_one() == expected
+                }
+            possibilities = next_possibilities
+        yield min(possibilities)
 
 
 Solver.run()
